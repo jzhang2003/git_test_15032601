2c428049fe5367e572c2e3b6f497ff717081a058
jzhang8 <jzhang8@ebay.com>
2015-03-26 11:05:34 +0800

    second commit

--- src/com/jzhang/study/Starter.java
+++ src/com/jzhang/study/Starter.java
@@ -2,6 +2,8 @@ package com.jzhang.study;
 
 public class Starter {
 	public static void main(String[] args) {
-		System.out.println("This is first verson");
+		System.out.println("This is first commit");
+		System.out.println("This is second commit");
+
 	}
 }
