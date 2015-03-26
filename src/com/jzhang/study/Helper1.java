aa0056a697bb03e9cb0c3244290d0e28b4013c61
jzhang8 <jzhang8@ebay.com>
2015-03-26 13:26:25 +0800

    third commit

---
+++ src/com/jzhang/study/Helper1.java
@@ -0,0 +1,5 @@
+package com.jzhang.study;
+
+public class Helper1 {
+
+}
--- src/com/jzhang/study/Starter.java
+++ src/com/jzhang/study/Starter.java
@@ -4,6 +4,8 @@ public class Starter {
 	public static void main(String[] args) {
 		System.out.println("This is first commit");
 		System.out.println("This is second commit");
+		System.out.println("This is third commit");
+
 
 	}
 }
